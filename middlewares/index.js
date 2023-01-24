7730fa5ed14f0df66d5a565db6f21abf405217ad
zlatan-dp <zlatandnepr@gmail.com>
2023-01-24 19:52:23 +0200

    implemented users avatar

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,6 +1,9 @@
 const createError = require("http-errors");
 const jwt = require("jsonwebtoken");
 
+const multer = require("multer");
+const path = require("path");
+
 const { User } = require("../models/user");
 const { SECRET_KEY } = process.env;
 
@@ -41,7 +44,23 @@ async function auth(req, res, next) {
   }
 }
 
+const tempDir = path.join(__dirname, "../temp");
+
+const multerConfig = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, tempDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: multerConfig,
+});
+
 module.exports = {
   validateBody,
   auth,
+  upload,
 };
